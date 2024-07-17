80532d3ac1303cd7122a6112db86f94dbffca88f
jdelara <Juan.deLara@uam.es>
2024-07-17 11:09:41 +0200

    - metamorphic testing

--- src/metamorphic/rules.py
+++ src/metamorphic/rules.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
+from types import SimpleNamespace
 
 from metamorphic.tests import Test
 
@@ -13,12 +14,11 @@ class Rule(BaseModel):
     then: str
 
     def test(self, tests: List[Test]):
-        print(f" - Checking rule {self.name}")
+        print(f" - Checking rule {self.name} [conversations: {self.conversations}]")
         if self.conversations == 1:
             self.__property_test(tests)
         else:
             self.__metamorphic_test(tests)
-
 
     def __property_test(self, tests: List[Test]):
         for test in tests:
@@ -33,18 +33,33 @@ class Rule(BaseModel):
                 else:
                     print(f"     -> Does not apply.")
 
-
     def __metamorphic_test(self, tests: List[Test]):
-        print(f"   - (to be implemented)")
-
+        # print(f"   - (to be implemented)")
+        for test1 in tests:
+            test_dict1 = test1.to_dict()
+            sns = SimpleNamespace(**test_dict1)
+            conv = [sns, sns]
+            test_dict = {'conv': conv}
+            for test2 in tests:
+                if test1 == test2:
+                    continue
+                test_dict2 = test2.to_dict()
+                conv[1] = SimpleNamespace(**test_dict2)
+                if self.applies(test_dict):
+                    print(f"   - On files: {test1.file_name}, {test2.file_name}")
+                    if self.if_eval(test_dict):
+                        if self.then_eval(test_dict):
+                            print(f"     -> Satisfied!")
+                        else:
+                            print(f"     -> NOT Satisfied!")
+                    else:
+                        print(f"     -> Does not apply.")
 
     def applies(self, test_dict: dict):
         return eval(self.when, test_dict)
 
-
     def if_eval(self, test_dict: dict):
         return eval(self.if_, test_dict)
-
 
     def then_eval(self, test_dict: dict):
         return eval(self.then, test_dict)

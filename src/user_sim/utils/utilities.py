c7f0fd6d8beef82658228df935d16b47911f1a1f
newi999 <alexdpe99@gmail.com>
2025-01-27 11:09:26 +0100

    updated requirements.txt

--- src/user_sim/utils/utilities.py
+++ src/user_sim/utils/utilities.py
@@ -38,16 +38,12 @@ def check_keys(key_list: list):
 check_keys(["OPENAI_API_KEY"])
 client = OpenAI()
 
+
 def parse_content_to_text(messages):
     return " ".join([message["content"] for message in messages if "content" in message])
 
-def get_encoding(encoded_file):
-    # with open(encoded_file, 'rb') as file:
-    #     result = chardet.detect(file.read())
-    #     encoding = result['encoding']
-    #     logger.info(f"Encoding detected: {encoding}")
-    #     return encoding
 
+def get_encoding(encoded_file):
     with open(encoded_file, 'rb') as file:
         detected = detect(file.read())
         return detected
